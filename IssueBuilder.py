29d66d58e7c3ffa982e78a3febfd8b2f9fcc605f
DestinyofYeet <olebend@gmail.com>
2020-12-28 23:12:19 +0100

    added check for cam and put in current date

--- IssueBuilder.py
+++ IssueBuilder.py
@@ -7,6 +7,7 @@ import subprocess
 import config
 import readhw
 from datetime import datetime
+import cv2
 
 
 def summary(lshwJson):
@@ -20,6 +21,14 @@ def category(lshwJson):
         return "Desktop"
 
 
+def check_for_cam(cam=0):
+    cap = cv2.VideoCapture(cam)
+    if cap is None or not cap.isOpened():
+        return False
+    else:
+        return True
+
+
 def build():
     
     lshwRaw = subprocess.check_output("sudo -S lshw -json", shell=True, universal_newlines=True)
@@ -28,9 +37,9 @@ def build():
     custom_fields = [
         {"field": {"name": "cpu"}, "value": readhw.cpu()},
         {"field": {"name": "Ram"}, "value": readhw.memory()},
-        {"field": {"name": "eingang"}, "value": datetime.now().strftime("%d.%m.%Y %H:%M")},  # heute
+        {"field": {"name": "eingang"}, "value": datetime.now().strftime("%d.%m.%Y")},  # heute
         {"field": {"name": "Festplatte"}, "value": ""},  # größe und typ
-        {"field": {"name": "webcam"}, "value": ""},  # irgendwie testen ob erkannt und geht
+        {"field": {"name": "webcam"}, "value": check_for_cam()},  # irgendwie testen ob erkannt und geht
         {"field": {"name": "wlan"}, "value": ""},  # irgendwie testen ob erkannt und geht
         {"field": {"name": "Standort"}, "value": config.STANDORT} 
     ]
