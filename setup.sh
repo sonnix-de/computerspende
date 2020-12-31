e668dace44cc41e804d75d1a7562d4b2529ea9b0
realwuffi <51153482+realwuffi@users.noreply.github.com>
2020-12-31 16:41:53 +0100

    Update setup.sh
    das -y muss weiter vorne stehen

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # python3 sollte schon installiert sein.
 # pip3 muss noch installiert werden
-sudo apt install python3-pip -Y
+sudo apt install -y python3-pip
 # das Menü benötigt noch eine Installation
 pip3 install -U -r requirements.txt
