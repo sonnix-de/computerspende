dc0d74b249df5ccc9190eef6b356064eb998a785
dmuiX <store@posteo.de>
2021-01-10 18:22:53 +0000

    update setup.sh to delete git clone (does not make sense)

--- setup.sh
+++ setup.sh
@@ -1,16 +1,6 @@
 # python3 sollte schon installiert sein.
 # pip3 muss noch installiert werden
 sudo apt install -y python3-pip git
-# Klonen des git repo des scriptes
-git clone https://github.com/sonnix-de/computerspende.git
 # das Script benötigt einige requirements
 
-cd computerspende 
-
 pip3 install -U -r requirements.txt
-
-python3 main.py
-
-cd ..
-
-rm -rf computerspende
