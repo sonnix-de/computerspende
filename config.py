5a0f0f97dfc3e4509569090ec40722a206545c2e
Anton <58666027+antonstech@users.noreply.github.com>
2021-02-11 14:53:18 +0100

    config.py fragt jetzt nach, ob es eine neue config.json datei erzeugen soll, wenn schon eine vorhanden ist.

--- config.py
+++ config.py
@@ -8,16 +8,24 @@ import json, os
 def createConfigFile():
     config = {}
     print("Es wird nun eine Konfigurationsdatei config.json erstellt.")
-    config['token'] = input("Gib deinen generierten Mantis API-Token ein:")
-    config['standort'] = input("Gib den Standort ein, der bei jedem Gerät automatisch gewählt werden soll:")
+    config['token'] = input("Gib deinen generierten Mantis API-Token ein: ")
+    config['standort'] = input("Gib den Standort ein, der bei jedem Gerät automatisch gewählt werden soll: ")
     config['mantisapi'] = "https://assets.computerspende-regensburg.de/api/rest/"
-    with open ('config.json', 'a+') as file:
-        json.dump(config, file)
+    with open ('config.json', 'w+') as file:
+        json.dump(config, file, indent=2)
+    print("Konfigurationsdatei erfolgreich erstellt.")
 
 
 def readConfig():
     global TOKEN, MANTIS_API, STANDORT
-    if not os.path.exists('config.json'):
+    if os.path.exists('config.json'):
+        print("Es ist bereits eine Konfigurationsdatei vorhanden.")
+        istvorhanden = input("Möchtest du eine neue erzeugen? (j/n): ")
+        if istvorhanden == "j":
+            createConfigFile()
+        else:
+            pass
+    else:
         createConfigFile()
     content = open("config.json").read()
     config = json.loads(content)
