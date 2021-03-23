4bafab9261f5bdaa99956ee876c2b3fdf327cf27
dmuiX <store@posteo.de>
2021-03-23 15:38:48 +0100

    Some changes at the setup scripts: - Added a setup-dev.sh script to get the correct dev env - added python3-venv - added -y to apt install

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # python3 sollte schon installiert sein.
 # pip3 muss noch installiert werden
 
-sudo apt install -y python3-pip
+sudo apt install -y python3-pip python3-venv
 # das Script benötigt einige requirements
 
 pip3 install -U -r requirements.txt
